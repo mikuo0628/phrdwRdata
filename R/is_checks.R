94049371c4b7107913e0ba4655ea6cfd98e68ed1
Mike Kuo <michael.kuo@bccdc.ca>
2023-12-05 15:43:28 -0800

    Update is.checks for clarity.

--- R/is_checks.R
+++ R/is_checks.R
@@ -1,4 +1,4 @@
 
-is.Query <- function(x) return(inherits(phrdw_obj, 'MDX_Qry'))
+is.Query <- function(x) return(inherits(x, 'MDX_Qry'))
 
-is.OlapConnection <- function(x) return(inherits(phrdw_obj, 'OLAP_Conn'))
+is.OlapConnection <- function(x) return(inherits(x, 'OLAP_Conn'))
