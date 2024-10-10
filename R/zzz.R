a685da1b917ed915f54b10e7b906495233807895
Mike Kuo <michael.kuo@bccdc.ca>
2024-10-10 14:55:46 -0700

    Change workflow from dependencies to renv

--- R/zzz.R
+++ R/zzz.R
@@ -4,9 +4,9 @@
 
   dyn.load(
     system.file(
-      # paste0('libs', Sys.getenv('R_ARCH'), '/phrdwRdata.dll'),
+      paste0('libs', Sys.getenv('R_ARCH'), '/phrdwRdata.dll'),
       # paste0('libs', '/x64', '/phrdwRdata.dll'),
-      paste0('libs', '/i386', '/phrdwRdata.dll'),
+      # paste0('libs', '/i386', '/phrdwRdata.dll'),
       # 'libs/phrdwRdata.dll',
       package = 'phrdwRdata'
     )
