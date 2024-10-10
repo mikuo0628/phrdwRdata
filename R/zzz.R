205f13e744581268d91febea6953e6b180bc91ee
Mike Kuo <michael.kuo@bccdc.ca>
2024-10-10 14:46:37 -0700

    Test i386 dll path

--- R/zzz.R
+++ R/zzz.R
@@ -5,7 +5,8 @@
   dyn.load(
     system.file(
       # paste0('libs', Sys.getenv('R_ARCH'), '/phrdwRdata.dll'),
-      paste0('libs', '/x64', '/phrdwRdata.dll'),
+      # paste0('libs', '/x64', '/phrdwRdata.dll'),
+      paste0('libs', '/i386', '/phrdwRdata.dll'),
       # 'libs/phrdwRdata.dll',
       package = 'phrdwRdata'
     )
