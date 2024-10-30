d299136651080abe2c082bf530f4d12aa6446807
Mike Kuo <michael.kuo@bccdc.ca>
2024-10-30 13:13:06 -0700

    Better handling of dll: OS based and will print warning message on attach

--- R/zzz.R
+++ R/zzz.R
@@ -2,16 +2,36 @@
 
   # packageStartupMessage()
 
-  dyn.load(
-    system.file(
-      file.path(
-        'libs',
-        ifelse(nchar(.Platform$r_arch) == 0, 'x64', .Platform$r_arch),
-        paste0('phrdwRdata', '.dll')
-      ),
-      package = 'phrdwRdata'
+  if (.Platform$OS.type == 'windows') {
+
+    dyn.load(
+      system.file(
+        file.path(
+          'libs',
+          ifelse(nchar(.Platform$r_arch) == 0, 'x64', .Platform$r_arch),
+          paste0('phrdwRdata', .Platform$dynlib.ext)
+        ),
+        package = 'phrdwRdata'
+      )
     )
-  )
+
+  }
+
+}
+
+.onAttach <- function(libname, pkgname) {
+
+  if (.Platform$OS.type != 'windows') {
+
+    packageStartupMessage(
+      paste(
+        'Please note: your OS is not Windows.',
+        'Data cubes will not be accessible.',
+        sep = '\n'
+      )
+    )
+
+  }
 
 }
 
