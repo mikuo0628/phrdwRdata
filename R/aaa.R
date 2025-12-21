7c4d0f0315fabf30aab50ca23b4b0abb7fee848d
Michael Kuo <michael.kuo@bccdc.ca>
2025-12-21 04:15:10 +0000

    Update reason for aaa.R - testing GH Codespace. Neat!

--- R/aaa.R
+++ R/aaa.R
@@ -1,1 +1,5 @@
+# Internal package environment
+# See https://r-pkgs.org/data.html
+# I am currently using this to set warnings for read_ssrs so 
+# it warns only once per session.
 the <- new.env(parent = emptyenv())
